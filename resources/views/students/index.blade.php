e0ef97366756eab80666d5ba8e5c345f2c2fdb17
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2022-08-03 11:05:18 +0700

    template v2

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,43 +1,44 @@
-<style>
-    .center {
-        text-align: center;
-    }
-</style>
-<h1>
-    Đây là danh sách sinh viên
-</h1>
-<a href="{{ route('student.create') }}">Add</a>
-<table border="1" width="100%">
-    <caption>
-        <form>
-            Search: <input type="text" name="q">
+@extends('layout.master')
+@section('content')
+    <div class='card-body'>
+        <a class="btn btn-success" href="{{ route('student.create') }}">
+            Add Student
+        </a>
+        <form class="float-right form-group form-inline">
+            <label class="mr-2">Search:</label>
+            <input type="search" name="q" value="{{ $search }}" class="form-control">
         </form>
-    </caption>
-    <tr>
-        <th>ID</th>
-        <th>Full Name</th>
+        <table class="table table-striped">
+            <tr>
+                <th>ID</th>
+                <th>Full Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Tool</th>
+            </tr>
+            @foreach ($students as $student)
+                <tr>
+                    <td class='center'>{{ $student->id }}</td>
+                    <td class='center'>{{ $student->fullname }}</td>
+                    <td class='center'>{{ $student->age }}</td>
+                    <td class='center'>{{ $student->gender }}</td>
+                    <td>
+                        <a href="{{ route('student.edit', $student) }}">Edit</a>
 
-        <th>Age</th>
-        <th>Gender</th>
-        <th>Tool</th>
-    </tr>
-    @foreach ($students as $student)
-        <tr>
-            <td class='center'>{{ $student->id }}</td>
-            <td class='center'>{{ $student->fullname }}</td>
-            <td class='center'>{{ $student->age }}</td>
-            <td class='center'>{{ $student->gender }}</td>
-            <td>
-                <a href="{{ route('student.edit', $student) }}">Edit</a>
+                        <form action="{{ route('student.destroy', $student) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                        </form>
+                    </td>
 
-                <form action="{{ route('student.destroy', $student) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
-            </td>
-
-        </tr>
-    @endforeach
-</table>
-{{ $students->links() }}
+                </tr>
+            @endforeach
+        </table>
+        <nav>
+            <ul class="pagination pagination-rounded mb-0">
+                {{ $students->links() }}
+            </ul>
+        </nav>
+    </div>
+@endsection
