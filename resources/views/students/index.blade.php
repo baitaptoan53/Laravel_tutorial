41feb56fb250ae001c6d7ffa30f334197367e22a
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2022-08-05 10:38:45 +0700

    fix datatables display

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -14,7 +14,6 @@
                     <th>ID</th>
                     <th>First Name</th>
                     <th>Last Name</th>
-                    <th>abc</th>
                     <th>Birth of date</th>
                     <th>Gender</th>
                     <th>Edit</th>
@@ -30,105 +29,90 @@
     <script type="text/javascript"
         src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.11.5/b-2.2.2/b-colvis-2.2.2/b-html5-2.2.2/b-print-2.2.2/date-1.1.2/fc-4.0.2/fh-3.2.2/r-2.2.9/rg-1.1.4/sc-2.0.5/sb-1.3.2/sl-1.3.4/datatables.min.js">
     </script>
-    <script>
-        < script >
-            $(function() {
-                var buttonCommon = {
-                    exportOptions: {
-                        columns: ':visible :not(.not-export)'
+   <script>
+    $(function() {
+        var buttonCommon = {
+            exportOptions: {
+                columns: ':visible :not(.not-export)'
+            }
+        };
+        let table = $('#table-index').DataTable({
+            dom: 'Blfrtip',
+            select: true,
+            buttons: [
+                $.extend( true, {}, buttonCommon, {
+                    extend: 'copyHtml5'
+                } ),
+                $.extend( true, {}, buttonCommon, {
+                    extend: 'csvHtml5'
+                } ),
+                $.extend( true, {}, buttonCommon, {
+                    extend: 'excelHtml5'
+                } ),
+                $.extend( true, {}, buttonCommon, {
+                    extend: 'pdfHtml5'
+                } ),
+                $.extend( true, {}, buttonCommon, {
+                    extend: 'print'
+                } ),
+                'colvis'
+            ],
+            processing: true,
+            serverSide: true,
+            ajax: '{!! route('student.api') !!}',
+            columnDefs: [
+                { className: "not-export", "targets": [ 3 ] }
+            ],
+            columns: [
+                { data: 'id', name: 'id' },
+                { data: 'fist_name', name: 'fist_name' },
+                { data: 'last_name', name: 'last_name' },
+            
+                { data: 'birthdate', name: 'birthdate' },
+                { data: 'gender', name: 'gender' },      
+                {
+                    data: 'edit',
+                    targets: 5,
+                    orderable: false,
+                    searchable: false,
+                    render: function ( data, type, row, meta ) {
+                      return `<a class="btn btn-primary" href="${data}">
+                            Edit
+                        </a>`;
                     }
-                };
-                let table = $('#table-index').DataTable({
-                    dom: 'Blfrtip',
-                    select: true,
-                    buttons: [
-                        $.extend(true, {}, buttonCommon, {
-                            extend: 'copyHtml5'
-                        }),
-                        $.extend(true, {}, buttonCommon, {
-                            extend: 'csvHtml5'
-                        }),
-                        $.extend(true, {}, buttonCommon, {
-                            extend: 'excelHtml5'
-                        }),
-                        $.extend(true, {}, buttonCommon, {
-                            extend: 'pdfHtml5'
-                        }),
-                        $.extend(true, {}, buttonCommon, {
-                            extend: 'print'
-                        }),
-                        'colvis'
-                    ],
-                    processing: true,
-                    serverSide: true,
-                    ajax: '{!! route('student.api') !!}',
-                    columnDefs: [{
-                        className: "not-export",
-                        "targets": [3]
-                    }],
-                    columns: [{
-                            data: 'id',
-                            name: 'id'
-                        },
-                        {
-                            data: 'fist_name',
-                            name: 'fist_name'
-                        },
-                        {
-                            data: 'last_name',
-                            name: 'last_name'
-                        },
-                        {
-                            data: 'birthdate',
-                            name: 'birthdate'
-                        },
-                        {
-                            data: 'gender',
-                            name: 'gender'
-                        },
-
-                        {
-                            data: 'edit',
-                            targets: 3,
-                            orderable: false,
-                            searchable: false,
-                            render: function(data, type, row, meta) {
-                                return `<a class="btn btn-primary" href="${student}">
-                                Edit
-                            </a>`;
-                            }
-                        },
-                        {
-                            data: 'destroy',
-                            targets: 4,
-                            orderable: false,
-                            searchable: false,
-                            render: function(data, type, row, meta) {
-                                return `<form action="${student}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type='button' class="btn-delete btn btn-danger">Delete</button>
-                            </form>`;
-                            }
-                        },
-                    ]
-                });
-                $(document).on('click', '.btn-delete', function() {
-                    let form = $(this).parents('form');
-                    $.ajax({
-                        url: form.attr('action'),
-                        type: 'POST',
-                        dataType: 'json',
-                        data: form.serialize(),
-                        success: function() {
-                            console.log("success");
-                            table.draw();
-                        },
-                        error: function() {
-                            console.log("error");
-                        }
-                    });
-                });
+                },
+                {
+                    data: 'destroy',
+                    targets: 6,
+                    orderable: false,
+                    searchable: false,
+                    render: function ( data, type, row, meta ) {
+                      return `<form action="${data}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type='button' class="btn-delete btn btn-danger">Delete</button>
+                        </form>`;
+                    }
+                },
+            ]
+        });
+        $(document).on('click','.btn-delete',function(){
+            let form = $(this).parents('form');
+            $.ajax({
+                url: form.attr('action'),
+                type: 'POST',
+                dataType: 'json',
+                data: form.serialize(),
+                success: function() {
+                    console.log("success");
+                    table.draw();
+                },
+                error: function() {
+                    console.log("error");
+                }
             });
-    </script>
+        });
+    });
+
+</script>
 @endpush
